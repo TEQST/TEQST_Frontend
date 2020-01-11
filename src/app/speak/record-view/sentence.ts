c7759c2e35530260a639a6023cb42570bfcc0cc7
Luca Huettner <luca.huettner@gmail.com>
2020-01-11 14:52:45 +0100

    feat: Create "Sentence" class

---
+++ src/app/speak/record-view/sentence.ts
@@ -0,0 +1,12 @@
+export class Sentence {
+    sentenceContent: String;
+    index: number;
+
+    getSentenceContent(): String {
+        return this.sentenceContent;
+    }
+
+    getIndex(): number {
+        return this.index;
+    }
+}
