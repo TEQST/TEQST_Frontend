06075b46477ab29ed97bef7d70abca5ee16c295a
Luca Huettner <luca.huettner@gmail.com>
2020-01-12 13:00:00 +0100

    feat: Add fetching of mock data to TextService

--- src/app/speak/record-view/text-service.service.ts
+++ src/app/speak/record-view/text-service.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Sentence } from './sentence';
 import { BehaviorSubject, ReplaySubject, of } from 'rxjs';
+import { TEXT } from './mock-text';
+import { RECORDINGINFO} from './mock-recording-information';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TextServiceService {
   private sentences = new ReplaySubject<Sentence[]>(1);
   private activeSentenceIndex = new BehaviorSubject<number>(1);
   private totalSentenceNumber = new ReplaySubject<number>(1);
   private furthestSentenceIndex = new BehaviorSubject<number>(1);
+
+  fetchText(): void {
+    of(TEXT).subscribe( text => {
+      this.totalSentenceNumber.next(text.totalSentenceNumber);
+      this.sentences.next(text.sentences);
+    })
+    of(RECORDINGINFO).subscribe( recording => {
+      this.activeSentenceIndex.next(recording.nextSentence);
+      this.furthestSentenceIndex.next(recording.nextSentence);
+    })
+  }
 
   getActiveSentenceIndex(): BehaviorSubject<number> {
     return this.activeSentenceIndex;
@@ -21,5 +35,9 @@ export class TextServiceService {
 
   getTotalSentenceNumber(): ReplaySubject<number> {
     return this.totalSentenceNumber;
+  }
+
+  constructor() {
+    this.fetchText();
   }
 }
