606f215c9cd211167c124268ce9e86ea4386a5a9
Luca Huettner <luca.huettner@gmail.com>
2020-01-12 12:58:47 +0100

    feat: Add  properties with getters to TextService

--- src/app/speak/record-view/text-service.service.ts
+++ src/app/speak/record-view/text-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Sentence } from './sentence';
+import { BehaviorSubject, ReplaySubject, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TextServiceService {
+  private sentences = new ReplaySubject<Sentence[]>(1);
+  private activeSentenceIndex = new BehaviorSubject<number>(1);
+  private totalSentenceNumber = new ReplaySubject<number>(1);
+  private furthestSentenceIndex = new BehaviorSubject<number>(1);
 
-  constructor() { }
+  getActiveSentenceIndex(): BehaviorSubject<number> {
+    return this.activeSentenceIndex;
+  }
+
+  getSentences(): ReplaySubject<Sentence[]> {
+    return this.sentences;
+  }
+
+  getTotalSentenceNumber(): ReplaySubject<number> {
+    return this.totalSentenceNumber;
+  }
 }
