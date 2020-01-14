3c6c5a8d02dee3e100e43217e6dd45013b843670
Luca Huettner <luca.huettner@gmail.com>
2020-01-14 19:46:45 +0100

    feat: Add AudioRecording service

---
+++ src/app/speak/text-list/record-view/audio-recording.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import * as RecordRTC from 'recordrtc';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AudioRecordingService {
+
+  constructor() {}
+
+}
