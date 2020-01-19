a46508d85599ebba2dd881518c259ed301eb3850
yxyx136 <linus.schilpp@gmail.com>
2020-01-19 12:42:45 +0100

    fix: change folder structure to better organize views and services

---
+++ src/app/views/settings/manage/manage.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage',
+  templateUrl: './manage.page.html',
+  styleUrls: ['./manage.page.scss'],
+})
+export class ManagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
