fb66bd2ca2ce0867f9bee8f6c8676e61c10fb468
Nicolas <46748047+Nicoalex@users.noreply.github.com>
2020-01-22 19:34:12 +0100

    style: centering login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -17,8 +17,8 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
   login(){
-    console.log("Username: "+ this.username);
-    console.log("Password: "+ this.password);
+    //console.log("Username: "+ this.username);
+   // console.log("Password: "+ this.password);
     this.navCtrl.navigateForward("speak");
   }
 
