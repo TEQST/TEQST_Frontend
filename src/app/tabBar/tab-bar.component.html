462a0994152fa188ddbec5837e9a58a05216fafb
Nicolas <46748047+Nicoalex@users.noreply.github.com>
2020-01-27 20:35:54 +0100

    feat: create TabBar Component

---
+++ src/app/tabBar/tab-bar.component.html
@@ -0,0 +1,5 @@
+<ion-footer>
+  <ion-toolbar>
+    <ion-title>Footer</ion-title>
+  </ion-toolbar>
+</ion-footer>
---
+++ src/app/tabBar/tab-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabBar',
+  templateUrl: './tab-bar.component.html',
+  styleUrls: ['./tab-bar.component.scss'],
+})
+export class TabBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
