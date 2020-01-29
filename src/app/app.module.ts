e6c2bdc9a426ee24738d5e5bc4076f9eda5e325d
Luca Huettner <luca.huettner@gmail.com>
2020-01-29 19:44:15 +0100

    fix: Error because of double http client declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http'
 
 
 
