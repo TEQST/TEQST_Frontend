5005b9a895cf5768e7effa91ccd3c75d461a6acd
Luca Huettner <luca.huettner@gmail.com>
2020-01-29 11:54:57 +0100

    feat: fetch Text  from Server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'; 
+
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -14,7 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,    
--- src/app/speak/folder-list/text-list/record-view/text-service.service.ts
+++ src/app/speak/folder-list/text-list/record-view/text-service.service.ts
@@ -1,38 +1,88 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, ReplaySubject, of, Observable } from 'rxjs';
 import { TEXT } from './mock-text';
-import { RECORDINGINFO} from './mock-recording-information';
+import { Text } from './text';
+import { RECORDINGINFO } from './mock-recording-information';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class TextServiceService {
+
   private sentences = new ReplaySubject<String[]>(1);
   // instantiate BehaviorSubjekts with 1 because every text has at least 1 sentence
   private activeSentenceIndex = new BehaviorSubject<number>(1);
   private totalSentenceNumber = new BehaviorSubject<number>(1);
   private furthestSentenceIndex = new BehaviorSubject<number>(1);
-  private hasRecording = new BehaviorSubject<boolean>(false);
+  private sentenceHasRecording = new BehaviorSubject<boolean>(false);
 
-  constructor() {
+  //Url Information
+  private textId = 2;
+  private baseUrl = "http://127.0.0.1:8000";
+  private textUrl = this.baseUrl + `/api/spk/texts/${this.textId}/`;
+  private getRecordingInfoUrl = this.baseUrl + `/api/textrecordings/?text=${this.textId}`;
+  private postRecordingInfoUrl = this.baseUrl + `/api/textrecordings/`;
+  private authToken = "Token 47cd8da0467088d9d682796c8b031b099b73cbcb";
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Authorization': this.authToken
+    })
+  };
+
+  constructor(private http: HttpClient) {
     this.fetchText();
   }
 
   fetchText(): void {
-    of(TEXT).subscribe(text => {
-      this.totalSentenceNumber.next(text.totalSentenceNumber);
-      this.sentences.next(text.sentences);
-    })
-    of(RECORDINGINFO).subscribe(recording => {
-      // only valid if text data is fetched first... because totalSentenceNumber would be wrong
-      this.activeSentenceIndex.next(Math.min(recording.nextSentence, this.totalSentenceNumber.getValue()));
-      this.furthestSentenceIndex.next(recording.nextSentence);
+    //fetch TextData from Server
+    this.http.get(this.textUrl, this.httpOptions).subscribe(text => {
+      this.totalSentenceNumber.next(text['content'].length);
+      this.sentences.next(text['content']);
+    });
+  }
+
+  setRecordingInfo(recordingInfo: Object) {
+    let index = recordingInfo[0]['active_sentence']
+    this.activeSentenceIndex.next(Math.min(index, this.totalSentenceNumber.getValue()));
+    this.furthestSentenceIndex.next(index);
+
+  }
+
+  async checkIfRecordingInfoExists(): Promise<boolean> {
+    let result = false;
+    await this.http.get(this.getRecordingInfoUrl, this.httpOptions).toPromise()
+    .then(info => {
+      if(info === null) {
+        console.log("false")
+        result = false;
+      } else {
+        console.log("true");
+        this.setRecordingInfo(info);
+        result = true;
+      }
+    });
+    //TODO
+    console.log(result)
+    return result
+  }
+
+  givePermissions(textToSpeech: boolean, speechRecognition: boolean ): void {
+    let recordingInfo = {
+      "text" : this.textId,
+      "TTS_permission": textToSpeech,
+      "SP_permission": speechRecognition
+    }
+    this.http.post(this.postRecordingInfoUrl, recordingInfo, this.httpOptions).subscribe(info => {
+      this.setRecordingInfo(info);
     })
   }
 
-  getHasRecording(): Observable<boolean> {
-    return this.hasRecording.asObservable();
+  getSentenceHasRecording(): Observable<boolean> {
+    return this.sentenceHasRecording.asObservable();
   }
 
   getActiveSentenceIndex(): BehaviorSubject<number> {
@@ -62,9 +112,9 @@ export class TextServiceService {
 
   private checkRecordingStatus(): void {
     if (this.activeSentenceIndex.getValue() < this.furthestSentenceIndex.getValue()) {
-      this.hasRecording.next(true)
+      this.sentenceHasRecording.next(true)
     } else {
-      this.hasRecording.next(false)
+      this.sentenceHasRecording.next(false)
     }
   }
 
