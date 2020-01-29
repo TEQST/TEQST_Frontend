0d990759e11f018476805b580c95c65ac2f69ce2
Luca Huettner <luca.huettner@gmail.com>
2020-01-29 11:55:49 +0100

    feat: Check if Textinfo exists

--- src/app/speak/folder-list/text-list/record-view/record-view.page.ts
+++ src/app/speak/folder-list/text-list/record-view/record-view.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 import { TextServiceService } from './text-service.service';
 
 @Component({
@@ -11,14 +13,62 @@ export class RecordViewPage implements OnInit {
   textId: string;
   hasRecording: boolean;
 
-  constructor(private route: ActivatedRoute, private textService: TextServiceService) {
-    textService.getHasRecording().subscribe((status) => this.hasRecording = status)
+  constructor(private route: ActivatedRoute, private textService: TextServiceService, private alertController: AlertController, private navCtrl: NavController) {
+    textService.getSentenceHasRecording().subscribe((status) => this.hasRecording = status)
    }
 
   ngOnInit() {
-    this.textId = this.route.snapshot.paramMap.get('publisherName');
-    
+    this.textId = this.route.snapshot.paramMap.get('publisherName'); 
+    this.textService.checkIfRecordingInfoExists().then(result => {
+      result ? '' : this.presentPermissionsCheckbox()
+    });
     // TODO: get text name from service and set as title
   }
 
+  async presentPermissionsCheckbox() {
+    const alert = await this.alertController.create({
+      header: 'Recording Permissions',
+      backdropDismiss: false,
+      message: "You have to select atleast one",
+      inputs: [
+        {
+          name: 'textToSpeech',
+          type: 'checkbox',
+          label: 'For Text to Speech',
+          value: 'TTS',
+        },
+
+        {
+          name: 'speechRecognition',
+          type: 'checkbox',
+          label: 'For Speech Recognition',
+          value: 'SR'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.navCtrl.back();
+          }
+        }, {
+          text: 'Ok',
+          handler: (permissions) => {
+            //check if atleast one option is selected
+            if(Object.keys(permissions).length === 0) {
+              this.navCtrl.back();
+            } else {
+              // Check which of the options is selected
+              let tts = Object.values(permissions).indexOf('TTS') > -1;
+              let sr = Object.values(permissions).indexOf('SR') > -1;
+              this.textService.givePermissions(tts, sr);
+            }
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
