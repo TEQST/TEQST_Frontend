d40794fbb506db5186c2207109ac413e9316e0e9
Nicolas <46748047+Nicoalex@users.noreply.github.com>
2020-01-29 14:37:52 +0100

    fix: reset httpOptions at login

--- src/app/services/usermgmt.service.ts
+++ src/app/services/usermgmt.service.ts
@@ -15,42 +15,41 @@ export class UsermgmtService {
   private authToken:string;
   private baseUrl = "http://127.0.0.1:8000";
 
-  private httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type':  'application/json',
-      'Authorization': 'authToken'
-    })
-  };
+  private httpOptions;
   constructor(public http: HttpClient, public navCtrl: NavController) { } 
 
 
   login(dataToSend) {
     let url = this.baseUrl +  "/api/auth/login/";
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'authToken'
+      })
+    };
     
     return this.http.post(url,dataToSend,this.httpOptions).subscribe((dataReturnFromServer: any)=>{
       this.dataFromServer = JSON.stringify(dataReturnFromServer);       
       this.authToken = "Token " + JSON.parse(this.dataFromServer).token;  
-      console.log("Token TEst:"+ this.authToken);
+
       this.httpOptions.headers = this.httpOptions.headers.set('Authorization', this.authToken); 
-      console.log(this.httpOptions.headers.get('Authorization')); 
-      console.log(this.httpOptions); 
+
       this.navCtrl.navigateForward("speak");
       },(error: any) => {       
         alert("wrong Password or Username")
       });
   }
+
   logout(){
     let url = this.baseUrl + "/api/auth/logout/";  
-    console.log("case 1"+ this.authToken); 
+  
      
     this.http.post(url, '', this.httpOptions).subscribe(() => {
       this.navCtrl.navigateForward("login");
       console.log("case 2"+ this.authToken);
     });
-    console.log(this.httpOptions);
-
-
   }
+
   public getToken(){
     return this.authToken;
   }
