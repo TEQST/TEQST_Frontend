225d1a76e155fc620d55aa8b0186f72d926a1e14
Nicolas <46748047+Nicoalex@users.noreply.github.com>
2020-01-30 15:51:34 +0100

    feat: Create LoadContent function

--- src/app/settings/profile/profile.page.ts
+++ src/app/settings/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsermgmtService } from '../../services/usermgmt.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,21 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
-  birthyear:null;
-  username:null;
-  language:null;
-  gender:null;
+  private dataFromServer:any="";
+  birthyear:number;
+  username:string="";
+  language:[];
+  gender:string="Male";
   education:null;
-  country:null;
+  country:string="Germany";
+  
 
-  constructor() { }
+  constructor(public usermgmtService:UsermgmtService) { }
 
   ngOnInit() {
   }
 
-  save(){
-    //TODO implement method to save changes 
+  loadContent(){
+   this.usermgmtService.loadContent().subscribe((dataReturnFromServer: any) => {
+    this.dataFromServer = JSON.stringify(dataReturnFromServer);
+    this.username = JSON.parse(this.dataFromServer).username;
+    this.birthyear = JSON.parse(this.dataFromServer).birth_year;
+    this.language = JSON.parse(this.dataFromServer).languages;
+    this.gender = JSON.parse(this.dataFromServer).gender;
+    this.education = JSON.parse(this.dataFromServer).education;
+    this.country = JSON.parse(this.dataFromServer).country;
+    console.log(this.birthyear);
+    console.log(this.language);
+    console.log(this.gender);
+    console.log(this.education);
+    console.log(this.country);
+   });  
   }
+
+  setUsername(newData){
+    this.username=newData;
+  }
+
+  save(){
+    var dataToSend = {birth_year:this.birthyear,language_ids:this.language, country:this.country}
+    console.log(this.language)
+    this.usermgmtService.updateProfile(dataToSend);
+  }
+
+  
 
 }
