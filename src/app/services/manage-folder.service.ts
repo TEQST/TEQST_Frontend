da1e055ee303c4987fb6eb2c884fc70cfe15ae3a
Luca Huettner <luca.huettner@gmail.com>
2020-01-30 19:11:35 +0100

    feat: Shared modal now interacts with the server

--- src/app/services/manage-folder.service.ts
+++ src/app/services/manage-folder.service.ts
@@ -2,16 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+interface User {
+  "id": number,
+  "username": string,
+  "education": string,
+  "gender": string,
+  "birth_year": number,
+  "languages": number[],
+  "country": null
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
+
 
 export class ManageFolderService {
 
   constructor(private http: HttpClient) { }
 
   SERVER_URL = 'http://localhost:8000'
-  AUTH_TOKEN = 'Token 15eb455f1c1d17fa69fe1738e58046b95b7524be'
+  AUTH_TOKEN = 'Token 4fdf7502a5618eafc29b88ab38463ed6dbf377ea'
 
   getSubfolderListFor(folderId: string): Observable<object> {
     let url = new URL(this.SERVER_URL + "/api/folders")
@@ -69,13 +81,30 @@ export class ManageFolderService {
 
   }
 
-  getSpeakers(): String[] {
-    let speakers = ["Alex", "Kevin", "Anna"];
-    return speakers;
+  getSpeakers(sharedfolderId: number) {
+    let url = new URL(this.SERVER_URL + `/api/sharedfolders/${sharedfolderId}/`)
+    return this.http.get<JSON[]>(url.toString(), {
+      headers: {
+        "Authorization": this.AUTH_TOKEN
+      }
+    });
   }
 
-  getAllUsers(): String[] {
-    let users = ["Ariel", "Marcel", "Alex", "Kevin", "Anna", "Isabelle", "Charline", "Gertrud", "Franz"];
-    return users;
+  setSpeakers(sharedfolderId: number, speakers: number[]) {
+    let url = new URL(this.SERVER_URL + `/api/sharedfolders/${sharedfolderId}/`);
+    return this.http.put<JSON>(url.toString(), { speaker_ids: speakers }, {
+      headers: {
+        "Authorization": this.AUTH_TOKEN
+      }
+    })
+  }
+
+  getAllUsers() {
+    let url = new URL(this.SERVER_URL + "/api/users/")
+    return this.http.get<User[]>(url.toString(), {
+      headers: {
+        "Authorization": this.AUTH_TOKEN
+      }
+    });
   }
 }
