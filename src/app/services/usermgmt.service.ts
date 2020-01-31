5ba0a050429c96809b39fecf93e07e0841ea13ad
Nicolas <46748047+Nicoalex@users.noreply.github.com>
2020-01-31 16:14:45 +0100

    feat: Create User Update function

--- src/app/services/usermgmt.service.ts
+++ src/app/services/usermgmt.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { NavController } from '@ionic/angular';
-import { BehaviorSubject, ReplaySubject, of, Observable } from 'rxjs';
-
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +11,8 @@ export class UsermgmtService {
   private dataFromServer:any="";
   private authToken:string;
   private baseUrl = "http://127.0.0.1:8000";
-  private httpOptions;
-  //private userInformation = new BehaviorSubject<User>();
-  
+  private httpOptions;  
 
-  private username;
-  private education;
-  private birthyear;
-  private gender;
   constructor(public http: HttpClient, public navCtrl: NavController) { } 
 
 
@@ -62,18 +54,16 @@ export class UsermgmtService {
 
   loadContent(){
     let url = this.baseUrl + "/api/user/"; 
+    // this.http.get(this.baseUrl+"api/langs",this.httpOptions).subscribe((dataReturnFromServer: any) => {
+    //   this.dataFromServer = JSON.parse(dataReturnFromServer); 
+    //   console.log(this.dataFromServer);
+    // });
     return this.http.get(url, this.httpOptions);
-    //.subscribe((dataReturnFromServer: any) => {
-       //this.dataFromServer = JSON.stringify(dataReturnFromServer);
-       //this.username = JSON.parse(this.dataFromServer).username;
-       
-       //this.education = JSON.parse(this.dataFromServer).education;
-       //this.gender = JSON.parse(this.dataFromServer).gender;
-       //this.birthyear = JSON.parse(this.dataFromServer).birth_year;    
-       //wie bekommt man die daten rüber zu profile.page.ts
-        
-   // });
+  }
 
+  getLangs(){
+    let url = this.baseUrl + "/api/langs/"; 
+    return this.http.get(url, this.httpOptions);
   }
   
   private reset(){
