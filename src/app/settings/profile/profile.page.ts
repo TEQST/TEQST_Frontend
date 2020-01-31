0d9eef85fad9ee37513b8b3db09a58802e1791a6
Nicolas <46748047+Nicoalex@users.noreply.github.com>
2020-01-31 16:14:15 +0100

    feat: create Load USer Content Function

--- src/app/settings/profile/profile.page.ts
+++ src/app/settings/profile/profile.page.ts
@@ -10,40 +10,50 @@ export class ProfilePage implements OnInit {
   private dataFromServer:any="";
   birthyear:number;
   username:string="";
-  language:[];
+  language:[object];
   gender:string="Male";
   education:null;
-  country:string="Germany";
+  country:string="Germany"; 
+  language_ids;
   
 
   constructor(public usermgmtService:UsermgmtService) { }
 
   ngOnInit() {
+    this.loadContent();
   }
 
   loadContent(){
-   this.usermgmtService.loadContent().subscribe((dataReturnFromServer: any) => {
+   this.usermgmtService.loadContent().subscribe((dataReturnFromServer:any) => {
     this.dataFromServer = JSON.stringify(dataReturnFromServer);
-    this.username = JSON.parse(this.dataFromServer).username;
-    this.birthyear = JSON.parse(this.dataFromServer).birth_year;
-    this.language = JSON.parse(this.dataFromServer).languages;
-    this.gender = JSON.parse(this.dataFromServer).gender;
-    this.education = JSON.parse(this.dataFromServer).education;
-    this.country = JSON.parse(this.dataFromServer).country;
+     //speichert die Sprachen in einem Array Sprache englisch ist arr[0]
+     this.language = dataReturnFromServer.languages;       
+   
+     this.username = JSON.parse(this.dataFromServer).username;
+     this.birthyear = JSON.parse(this.dataFromServer).birth_year;    
+     //this.language = this.language["english_name"];
+     this.gender = JSON.parse(this.dataFromServer).gender;
+     this.education = JSON.parse(this.dataFromServer).education;
+     this.country = JSON.parse(this.dataFromServer).country;
     console.log(this.birthyear);
-    console.log(this.language);
+    console.log(this.language[0]);
+    
     console.log(this.gender);
     console.log(this.education);
     console.log(this.country);
    });  
   }
-
-  setUsername(newData){
-    this.username=newData;
+  getAllLangs(){
+    this.usermgmtService.getLangs().subscribe((dataReturnFromServer: any) => {
+      this.dataFromServer = JSON.stringify(dataReturnFromServer);
+      console.log(this.dataFromServer);
+    })
   }
 
+  
+
   save(){
-    var dataToSend = {birth_year:this.birthyear,language_ids:this.language, country:this.country}
+    var dataToSend = {birth_year:this.birthyear,language_ids:this.language_ids, country:this.country}
     console.log(this.language)
     this.usermgmtService.updateProfile(dataToSend);
   }
