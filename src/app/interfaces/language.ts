99714b892d6defb5803014f9793d934a004a348c
Luca Huettner <luca.huettner@gmail.com>
2020-03-27 17:05:10 +0100

    feat: Add user interface

---
+++ src/app/interfaces/language.ts
@@ -0,0 +1,7 @@
+export interface Language {
+    english_name: string;
+    native_name: string;
+    short: string;
+    right_to_left: boolean;
+    is_menu_language: boolean;
+}
---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,15 @@
+import { Language } from './language';
+
+export interface User {
+    id: number;
+    username: string;
+    education: string;
+    gender: string;
+    birth_year: number;
+    languages: Language[];
+    menu_language: Language[];
+    accent: string;
+    country: string;
+    is_publisher: boolean;
+}
+}
