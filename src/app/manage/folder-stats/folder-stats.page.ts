78ee376730926a5713d06ebfa664317f53f0c9a0
Luca Huettner <luca.huettner@gmail.com>
2020-04-11 15:19:56 +0200

    feat: Implement statistics for shared folder

--- src/app/manage/folder-stats/folder-stats.page.ts
+++ src/app/manage/folder-stats/folder-stats.page.ts
@@ -1,4 +1,7 @@
+import { FolderStats } from './../../interfaces/folder-stats';
+import { StatisticsService } from './../../services/statistics.service';
 import { Component, OnInit, Input } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-folder-stats',
@@ -10,9 +13,22 @@ export class FolderStatsPage implements OnInit {
   @Input() folderId: number;
   @Input() folderName: string;
 
-  constructor() { }
+  public folderStats: FolderStats = {
+    id: 0,
+    name: 'Loading',
+    speakers: []
+  };
+
+  constructor(private statsServices: StatisticsService, private viewCtrl: ModalController) { }
 
   ngOnInit() {
+    this.statsServices.getSharedFolderStats(this.folderId).subscribe((folderStats) => {
+      this.folderStats = folderStats;
+    });
+  }
+
+  public dismiss() {
+    this.viewCtrl.dismiss();
   }
 
 }
