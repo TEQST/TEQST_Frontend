5f43c6ca78b6fa63843ab0699c0892b81a272754
Nicolas <46748047+Nicoalex@users.noreply.github.com>
2020-05-12 20:06:58 +0200

    fix: change Content Type only when no Conntent Type is given

--- src/app/interceptors/server-authtoken-interceptor.service.ts
+++ src/app/interceptors/server-authtoken-interceptor.service.ts
@@ -12,9 +12,17 @@ export class ServerAuthtokenInterceptorService implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler) {
     const authToken = localStorage.getItem('Token');
-    const authReq = request.clone({
-      setHeaders: { Authorization: authToken}
-    });
+    let authReq;
+    if (request.headers.get('Content-Type') !== null || request.urlWithParams ) {
+      authReq = request.clone({
+        setHeaders: { Authorization: authToken}
+      });
+    } else {
+      authReq = request.clone({
+        setHeaders: { Authorization: authToken,
+           'Content-Type': 'application/json'}
+      });
+    }
     return next.handle(authReq);
 
   }
