0d1e72049f42f6eff38817bde46788f77c2c9201
Luca Hüttner <35100303+lucahuettner@users.noreply.github.com>
2020-06-08 15:13:32 +0200

    feat: Add interfaces for the shared folder request

---
+++ src/app/interfaces/shared-folder.ts
@@ -0,0 +1,19 @@
+import { TimeStats } from "./time-stats";
+
+
+export interface SharedFolder {
+
+    id: number;
+    name: string;
+    owner: number;
+    path: string;
+    timestats: TimeStats;
+    texts: SharedFolderText[];
+}
+
+interface SharedFolderText {
+    id: number;
+    title: string;
+}
+
+
---
+++ src/app/interfaces/time-stats.ts
@@ -0,0 +1,4 @@
+export interface TimeStats {
+    rec_time_without_rep: number;
+    rec_time_with_rep: number;
+}
