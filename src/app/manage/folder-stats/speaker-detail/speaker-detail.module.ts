ee6bb55fd6ba29860fea971959bdd64d022743cf
Luca Hüttner <35100303+lucahuettner@users.noreply.github.com>
2020-06-15 11:45:21 +0200

    fix: Import missing TranslateModule

--- src/app/manage/folder-stats/speaker-detail/speaker-detail.module.ts
+++ src/app/manage/folder-stats/speaker-detail/speaker-detail.module.ts
@@ -1,3 +1,4 @@
+import { TranslateModule } from '@ngx-translate/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -13,7 +14,8 @@ import { SpeakerDetailPage } from './speaker-detail.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    SpeakerDetailPageRoutingModule
+    SpeakerDetailPageRoutingModule,
+    TranslateModule
   ],
   declarations: [SpeakerDetailPage]
 })
--- src/app/manage/folder-stats/speaker-list/speaker-list.module.ts
+++ src/app/manage/folder-stats/speaker-list/speaker-list.module.ts
@@ -1,3 +1,4 @@
+import { TranslateModule } from '@ngx-translate/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -13,7 +14,8 @@ import { SpeakerListPage } from './speaker-list.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    SpeakerListPageRoutingModule
+    SpeakerListPageRoutingModule,
+    TranslateModule
   ],
   declarations: [SpeakerListPage]
 })
