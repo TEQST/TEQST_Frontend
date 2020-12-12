8dec95cb3f8dd84e59f4b4d7aacfee89a52a430c
Luca Hüttner <35100303+lucahuettner@users.noreply.github.com>
2020-12-12 15:02:56 +0100

    fix: Loading Bar not disappearing when an error occurs

---
+++ src/app/interceptors/loader-interceptor.service.ts
@@ -0,0 +1,48 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { LoaderService } from '../services/loader.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoaderInterceptorService implements HttpInterceptor{
+
+  private requests: HttpRequest<any>[] = []
+
+  constructor(public loaderService: LoaderService) { }
+
+  removeRequest(req: HttpRequest<any>) {
+    const i = this.requests.indexOf(req);
+    if (i >= 0) {
+      this.requests.splice(i, 1);
+    }
+    if (this.requests.length === 0) {
+      this.loaderService.hide()
+    }
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler):
+      Observable<HttpEvent<any>> {
+      
+    this.requests.push(request)
+    this.loaderService.show();
+
+    return next.handle(request).pipe(
+        tap({
+          next: event => {
+            if (event instanceof HttpResponse) {
+              this.removeRequest(request);
+            }
+          },
+          error: err => {
+            this.removeRequest(request);
+          },
+          complete: () => {
+            this.removeRequest(request);
+          }
+        }),
+    );
+  }
+}
