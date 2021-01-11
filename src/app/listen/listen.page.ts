c78adf7dba34d722b41a04947deb9997bcdc6055
sugeedarou <linus.schilpp@googlemail.com>
2021-01-11 19:11:36 +0100

    feat: add listener tab

---
+++ src/app/listen/listen.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listen',
+  templateUrl: './listen.page.html',
+  styleUrls: ['./listen.page.scss'],
+})
+export class ListenPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
