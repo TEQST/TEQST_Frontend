9931a93a1af4850c7694dd40f44a764bf198eb37
Luca Hüttner <35100303+lucahuettner@users.noreply.github.com>
2021-03-19 21:32:48 +0100

    feat: Rework profile data handling + add email field

--- src/app/settings/profile/profile.module.ts
+++ src/app/settings/profile/profile.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {TranslateModule} from '@ngx-translate/core';
 
@@ -18,6 +18,7 @@ import {ProfilePage} from './profile.page';
     IonicModule,
     ProfilePageRoutingModule,
     TranslateModule,
+    ReactiveFormsModule,
   ],
   declarations: [ProfilePage],
 })
