9451634492018f2374f8d1fe7adaa3c7d002d718
sugeedarou <6258568+sugeedarou@users.noreply.github.com>
2022-05-04 15:44:56 +0200

    Ionic6 (#5)
    * install yarn dependency
    
    * feat: upgrade to ionic 6 and angular 13
    
    * feat: upgrade angular-toolkit
    
    * finally fix backend routing
    
    * fix getListener
    
    * task filter info popover
    
    Co-authored-by: YAWNICK <jaweiss1999@googlemail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,58 +27,57 @@ import {ServerAuthtokenInterceptorService}
   from './interceptors/server-authtoken-interceptor.service';
 
 @NgModule({
-  exports: [
-    TranslateModule,
-  ],
-  declarations: [
-    AppComponent,
-    PageNotFoundComponent,
-  ],
-  entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    HttpClientModule,
-    HammerModule,
-    FormsModule,
-    ReactiveFormsModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-    }),
-    ServiceWorkerModule
-        .register('ngsw-worker.js', {enabled: environment.production}),
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    UsernameValidator,
-    {
-      provide: RollbarService,
-      useFactory: rollbarFactory,
-    },
-    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ServerErrorInterceptorService,
-      multi: true,
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: LoaderInterceptorService,
-      multi: true,
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ServerAuthtokenInterceptorService,
-      multi: true,
-    },
-  ],
-  bootstrap: [AppComponent],
+    exports: [
+        TranslateModule,
+    ],
+    declarations: [
+        AppComponent,
+        PageNotFoundComponent,
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        HttpClientModule,
+        HammerModule,
+        FormsModule,
+        ReactiveFormsModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient],
+            },
+        }),
+        ServiceWorkerModule
+            .register('ngsw-worker.js', { enabled: environment.production }),
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        UsernameValidator,
+        {
+            provide: RollbarService,
+            useFactory: rollbarFactory,
+        },
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ServerErrorInterceptorService,
+            multi: true,
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: LoaderInterceptorService,
+            multi: true,
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ServerAuthtokenInterceptorService,
+            multi: true,
+        },
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
 
