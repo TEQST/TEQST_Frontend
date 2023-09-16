1f1f70e7335aa676e9df8d54efe6bc0db69b2f03
Doomse <ueqnn@student.kit.edu>
2023-09-16 17:58:23 +0200

    Update download explainer

--- src/app/manage/download-folder/download-content/download-content.page.ts
+++ src/app/manage/download-folder/download-content/download-content.page.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 import { Folder } from '../../manage.folder';
 import { Constants } from 'src/app/constants';
 
@@ -14,8 +14,17 @@ export class DownloadContentPage implements OnInit {
 
   server: string;
 
+  wget_cmd = "wget --recursive --content-disposition --no-host-directories --cut-dirs=2 --reject=\"index.html*\"";
+  wget2_cmd = "wget2 --recursive --no-host-directories --cut-dirs=2 --cut-file-get-vars --reject=\"index.html\"";
+
+  key_url = "url";
+  key_wg1 = "wg1";
+  key_wg2 = "wg2";
+
   constructor(
+    private toastController: ToastController,
     private viewCtrl: ModalController) {
+      // Grab base url for backend server
       if (Constants.SERVER_URL) {
         this.server = Constants.SERVER_URL;
       } else {
@@ -28,6 +37,34 @@ export class DownloadContentPage implements OnInit {
 
   dismiss() {
     this.viewCtrl.dismiss()
+  }
+
+  getContent(key: string): string {
+    switch (key) {
+      case this.key_url:
+        return this.server + this.folder.dlpath;
+      case this.key_wg1:
+        return `${this.wget_cmd} \"${this.server}${this.folder.dlpath}\"`;
+      case this.key_wg2:
+        return `${this.wget2_cmd} \"${this.server}${this.folder.dlpath}\"`;
+      default:
+        return "";
+    }
+  }
+
+  async copyLink($event, key: string): Promise<void> {
+    $event.preventDefault();
+    $event.stopPropagation();
+    navigator.clipboard.writeText( this.getContent(key) )
+    const toast = await this.toastController.create({
+      message: 'Successfully copied to clipboard!',
+      icon: 'checkmark',
+      color: 'success',
+      position: 'bottom',
+      animated: true,
+      duration: 1000,
+    });
+    await toast.present();
   }
 
 }
